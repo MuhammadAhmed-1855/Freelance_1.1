a7830852850b5a7b0c8d4e73c4957e02d6284f8c
MuhammadAhmed-1855 <i201855@nu.edu.pk>
2024-01-30 17:26:17 +0500

    Contact

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Hero from "../../components/Hero";
 import Faq from "../../components/Faq";
 import Works from "../../components/Works";
+import Contact from "../../components/Contact";
 
 const Home = () => {
     return (
@@ -14,6 +15,8 @@ const Home = () => {
             <Works />
             {/* <hr /> */}
             <Faq />
+            {/* <hr /> */}
+            <Contact />
         </div>
     );
 };
